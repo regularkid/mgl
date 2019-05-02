e5756a8faaa987b9f9bf7d629435227e1ec1ba2f
Bryan Perfetto <regularkid@gmail.com>
2019-05-01 22:08:24 -0700

    Stress testing how many triangles can be drawn per frame.

---
+++ src/main.js
@@ -0,0 +1,38 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d", { alpha: false });
+var lastTime = 0;
+
+function GameLoop(curTime)
+{
+    let dt = Math.min((curTime - lastTime) / 1000.0, 0.2);	
+    lastTime = curTime;
+
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    let numTris = 1000;
+    let colors = ["#F00", "#0F0", "#00F", "#FF0", "#F0F", "#0FF"];
+    for (let i = 0; i < numTris; i++)
+    {
+        let randColorIdx = Math.floor(Math.random()*colors.length);
+        ctx.fillStyle = colors[randColorIdx];
+        ctx.beginPath();
+
+        let x = Math.random()*canvas.width;
+        let y = Math.random()*canvas.height;
+        ctx.moveTo(x, y);
+        ctx.lineTo(x + Math.random()*100, y + Math.random()*100);
+        ctx.lineTo(x + Math.random()*100, y + Math.random()*100);
+        ctx.fill();
+    }
+
+    //TEMP!	
+    ctx.font = `Bold 16px Arial`;	
+    ctx.fillStyle = "#FFF";	
+    let fps = 1.0 / dt;
+    ctx.fillText(`${Math.floor(fps)}`, 10, 20);
+
+    window.requestAnimationFrame(GameLoop);
+}
+
+window.requestAnimationFrame(GameLoop);
