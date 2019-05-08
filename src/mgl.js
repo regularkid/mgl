0c6c917ddc62eb40bfd2fa4199b49cef6bf64ef7
Bryan Perfetto <regularkid@gmail.com>
2019-05-07 22:11:36 -0700

    Switched tri rasterization algorithm to use 2D cross-product and setup barycentric coordinates.

--- src/mgl.js
+++ src/mgl.js
@@ -35,49 +35,83 @@ class MGL
         {
             let poly = this.polys[i];
 
-            let points = [poly.pa, poly.pb, poly.pc];
-            points.sort((a, b) => b.y - a.y);
+            let xMin = Math.round(Math.min(poly.pa.x, Math.min(poly.pb.x, poly.pc.x)));
+            let xMax = Math.round(Math.max(poly.pa.x, Math.max(poly.pb.x, poly.pc.x)));
+            let yMin = Math.round(Math.min(poly.pa.y, Math.min(poly.pb.y, poly.pc.y)));
+            let yMax = Math.round(Math.max(poly.pa.y, Math.max(poly.pb.y, poly.pc.y)));
 
-            let top = Math.round(points[0].y);
-            let middle = Math.round(points[1].y);
-            let bottom = Math.round(points[2].y);
-
-            let totalHeight = top - bottom;
-            let bottomToMiddleHeight = middle - bottom;
-            let middleToTopHeight = top - middle;
-            let yOffset = 0;
-            let tLeft = 0;
-            let left = 0;
-            let tRight = 0;
-            let right = 0;
-            for (let y = bottom; y <= top; y++)
+            let p = new Vec3(0, 0, 0);
+            let w0 = 0;
+            let w1 = 0;
+            let w2 = 0;
+            let triArea2Reciprocal = 1.0 / this.SignedParallelogramArea(poly.pa, poly.pb, poly.pc);
+            for (let y = yMin; y <= yMax; y++)
             {
-                yOffset = y - bottom;
-
-                tLeft = yOffset / totalHeight;
-                left = Math.round(points[2].x + (points[0].x - points[2].x)*tLeft);
-
-                if (y >= middle)
+                for (let x = xMin; x <= xMax; x++)
                 {
-                    tRight = (y - middle) / middleToTopHeight;
-                    right = Math.round(points[1].x + (points[0].x - points[1].x)*tRight);
-                }
-                else
-                {
-                    tRight = yOffset / bottomToMiddleHeight;
-                    right = Math.round(points[2].x + (points[1].x - points[2].x)*tRight);
-                }
+                    p.x = x;
+                    p.y = y;
 
-                if (right < left) { let t = left; left = right; right = t; }
 
-                let pixelIdx = (y * this.canvas.width) + left;
-                let pixelIdxEnd = (y * this.canvas.width) + right;
-                while (pixelIdx <= pixelIdxEnd)
-                {
-                    this.framebuffer32Bit[pixelIdx] = poly.paColor;
-                    pixelIdx++;
+                    w0 = this.SignedParallelogramArea(poly.pa, poly.pb, p);
+                    w1 = this.SignedParallelogramArea(poly.pb, poly.pc, p);
+                    w2 = this.SignedParallelogramArea(poly.pc, poly.pa, p);
+
+                    if (w0 > 0.0 && w1 > 0.0 && w2 > 0.0)
+                    {
+                        // Convert to barycentric coordinates
+                        w0 *= triArea2Reciprocal;
+                        w1 *= triArea2Reciprocal;
+                        w2 *= triArea2Reciprocal;
+
+                        this.framebuffer32Bit[(y * this.canvas.width) + x] = poly.paColor;//*w0 + poly.pbColor*w1 + poly.pcColor*w2;
+                    }
                 }
             }
+
+            // let points = [poly.pa, poly.pb, poly.pc];
+            // points.sort((a, b) => b.y - a.y);
+
+            // let top = Math.round(points[0].y);
+            // let middle = Math.round(points[1].y);
+            // let bottom = Math.round(points[2].y);
+
+            // let totalHeight = top - bottom;
+            // let bottomToMiddleHeight = middle - bottom;
+            // let middleToTopHeight = top - middle;
+            // let yOffset = 0;
+            // let tLeft = 0;
+            // let left = 0;
+            // let tRight = 0;
+            // let right = 0;
+            // for (let y = bottom; y <= top; y++)
+            // {
+            //     yOffset = y - bottom;
+
+            //     tLeft = yOffset / totalHeight;
+            //     left = Math.round(points[2].x + (points[0].x - points[2].x)*tLeft);
+
+            //     if (y >= middle)
+            //     {
+            //         tRight = (y - middle) / middleToTopHeight;
+            //         right = Math.round(points[1].x + (points[0].x - points[1].x)*tRight);
+            //     }
+            //     else
+            //     {
+            //         tRight = yOffset / bottomToMiddleHeight;
+            //         right = Math.round(points[2].x + (points[1].x - points[2].x)*tRight);
+            //     }
+
+            //     if (right < left) { let t = left; left = right; right = t; }
+
+            //     let pixelIdx = (y * this.canvas.width) + left;
+            //     let pixelIdxEnd = (y * this.canvas.width) + right;
+            //     while (pixelIdx <= pixelIdxEnd)
+            //     {
+            //         this.framebuffer32Bit[pixelIdx] = poly.paColor;
+            //         pixelIdx++;
+            //     }
+            // }
         }
 
         this.ctx.putImageData(this.framebuffer, 0, 0);
@@ -108,5 +142,10 @@ class MGL
                                         obj.colors[obj.indices[i + 1]],
                                         obj.colors[obj.indices[i + 2]]));
         }
+    }
+
+    SignedParallelogramArea(pa, pb, pc) 
+    { 
+        return (pc.x - pa.x)*(pb.y - pa.y) - (pc.y - pa.y)*(pb.x - pa.x); 
     }
 }
